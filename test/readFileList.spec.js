318918eb3b722e84c8948d18ccbcd580f9bf8f19
wubaiqing <wubaiqing@vip.qq.com>
2019-01-23 13:37:00 +0800

    fix: 修改测试的描述信息

--- test/readFileList.spec.js
+++ test/readFileList.spec.js
@@ -1,9 +1,9 @@
 import readFileList from "../scripts/build";
 
-test("is directory empty", () => {
+test("This directory doesn't exists.", () => {
   expect(readFileList("2019", "13")).toEqual([]);
 });
 
-test("not empty", () => {
+test("Directory exists, files are not empty.", () => {
   expect(readFileList("2019", "01").length).not.toBe(0);
 });
