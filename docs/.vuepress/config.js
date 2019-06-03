dd74b57ce69d9609af5ae185a1b7833ad6931d99
wubaiqing (琉璃) <wubaiqing@vip.qq.com>
2019-06-03 13:14:23 +0800

    feat: 新增6月周报左侧菜单

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -38,6 +38,11 @@ module.exports = {
     sidebar: [
       ["/", "1. 介绍"],
       {
+        title: "2019年06月",
+        collapsable: false,
+        children: readFileList('2019', '06')
+      },
+      {
         title: "2019年05月",
         collapsable: false,
         children: readFileList('2019', '05')
