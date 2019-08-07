b77733a044e14e39ba8af72c06ebed78318d22fc
wubaiqing (琉璃) <wubaiqing@vip.qq.com>
2019-08-07 15:11:54 +0800

    fix: #52-the title has

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,99 +1,115 @@
-const readFileList = require('../../scripts/build.js');
+const readFileList = require("../../scripts/build.js");
 
 module.exports = {
   title: "每日时报",
-  description: "每日时报，会以前端技术体系为主要分享课题。内容会以：文章、工具、新闻、视频几大板块作为主要分类。",
+  description:
+    "每日时报，会以前端技术体系为主要分享课题。内容会以：文章、工具、新闻、视频几大板块作为主要分类。",
   head: [
-    ['link', { rel: 'alternate', type: 'application/rss+xml', href: '/rss.xml', title: '每日时报' }]
+    [
+      "link",
+      { rel: "alternate", type: "application/rss+xml", href: "/rss.xml", title: "每日时报" },
+    ],
   ],
-  ga: 'UA-131469836-1',
+  ga: "UA-131469836-1",
   plugins: [
-    ['@vuepress/google-analytics'],
-    ['vuepress-plugin-rss-support', {
-      site_url: 'https://wubaiqing.github.io/zaobao',
-      filter: page => /^\/201.+/.test(page.path),
-      copyright: '2019每日时报',
-      count: 60
-    }],
-    ['@vssue/vuepress-plugin-vssue', {
-      platform: 'github',
-      owner: 'wubaiqing',
-      repo: 'zaobao',
-      clientId: '20ee116870a4be78bb37',
-      clientSecret: '13c39d03e4bdc6f13023cc6f318ca132c7522b9f',
-    }],
+    ["@vuepress/google-analytics"],
+    [
+      "vuepress-plugin-rss-support",
+      {
+        site_url: "https://wubaiqing.github.io/zaobao",
+        filter: page => /^\/201.+/.test(page.path),
+        copyright: "2019每日时报",
+        count: 60,
+      },
+    ],
+    [
+      "@vssue/vuepress-plugin-vssue",
+      {
+        platform: "github",
+        owner: "wubaiqing",
+        repo: "zaobao",
+        clientId: "20ee116870a4be78bb37",
+        clientSecret: "13c39d03e4bdc6f13023cc6f318ca132c7522b9f",
+      },
+    ],
   ],
-  base: '/zaobao/',
+  base: "/zaobao/",
   themeConfig: {
     repo: "wubaiqing/zaobao",
-    lastUpdated: 'Last Updated',
-    docsDir: 'docs',
+    lastUpdated: "Last Updated",
+    docsDir: "docs",
     algolia: {
-      apiKey: 'd9708b4d74ba98295f2a87341fae3f0c',
-      indexName: 'zaobao'
+      apiKey: "d9708b4d74ba98295f2a87341fae3f0c",
+      indexName: "zaobao",
     },
-    nav: [
-      { text: '留言', link: '/other/talk' },
-    ],
+    nav: [{ text: "留言", link: "/other/talk" }],
     sidebar: [
       ["/", "1. 介绍"],
       {
-        title: "2019年08月",
+        title: "2019年08月",
         collapsable: false,
-        children: readFileList('2019', '08')
+        children: readFileList("2019", "08"),
       },
       {
-        title: "2019年07月",
+        title: "2019年07月",
         collapsable: false,
-        children: readFileList('2019', '07')
+        children: readFileList("2019", "07"),
       },
       {
-        title: "2019年06月",
+        title: "2019年06月",
         collapsable: false,
-        children: readFileList('2019', '06')
+        children: readFileList("2019", "06"),
       },
       {
-        title: "2019年05月",
+        title: "2019年05月",
         collapsable: true,
-        children: readFileList('2019', '05')
+        children: readFileList("2019", "05"),
       },
       {
-        title: "2019年04月",
+        title: "2019年04月",
         collapsable: true,
-        children: readFileList('2019', '04')
-      }, {
-        title: "2019年03月",
+        children: readFileList("2019", "04"),
+      },
+      {
+        title: "2019年03月",
         collapsable: true,
-        children: readFileList('2019', '03')
-      }, {
-        title: "2019年02月",
+        children: readFileList("2019", "03"),
+      },
+      {
+        title: "2019年02月",
         collapsable: true,
-        children: readFileList('2019', '02')
-      }, {
-        title: "2019年01月",
+        children: readFileList("2019", "02"),
+      },
+      {
+        title: "2019年01月",
         collapsable: true,
-        children: readFileList('2019', '01')
-      }, {
-        title: "2018年12月",
+        children: readFileList("2019", "01"),
+      },
+      {
+        title: "2018年12月",
         collapsable: true,
-        children: readFileList('2018', '12')
-      }, {
-        title: "2018年11月",
+        children: readFileList("2018", "12"),
+      },
+      {
+        title: "2018年11月",
         collapsable: true,
-        children: readFileList('2018', '11')
-      }, {
-        title: "2018年10月",
+        children: readFileList("2018", "11"),
+      },
+      {
+        title: "2018年10月",
         collapsable: true,
-        children: readFileList('2018', '10')
-      }, {
-        title: "2018年09月",
+        children: readFileList("2018", "10"),
+      },
+      {
+        title: "2018年09月",
         collapsable: true,
-        children: readFileList('2018', '09')
-      }, {
-        title: "2018年08月",
+        children: readFileList("2018", "09"),
+      },
+      {
+        title: "2018年08月",
         collapsable: true,
-        children: readFileList('2018', '08')
-      }
+        children: readFileList("2018", "08"),
+      },
     ],
-  }
+  },
 };
