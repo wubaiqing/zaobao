c652f1b93c0b8a2eab575c649a9284c105725d41
xxholly32 <xxholly32@163.com>
2020-07-01 23:14:28 +0800

    feat: 新增 7月2日 早报

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -48,6 +48,11 @@ module.exports = {
     sidebar: [
       ["/", "1. 介绍"],
       {
+        title: "2020年07月",
+        collapsable: false,
+        children: readFileList("2020", "07"),
+      },
+      {
         title: "2020年06月",
         collapsable: false,
         children: readFileList("2020", "06"),
