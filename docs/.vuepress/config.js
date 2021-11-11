39c78c25a6b476c4476931fa407c438ac22b34d4
wubaiqing <wubaiqing@vip.qq.com>
2021-11-11 14:04:44 +0800

    fix: 折叠 9 月日报

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -67,7 +67,7 @@ module.exports = {
       },
       {
         title: "2021年09月",
-        collapsable: false,
+        collapsable: true,
         children: readFileList("2021", "09")
       },
       {
