d770d961d5aadd2653706715ff3783a1933b5df3
wubaiqing <wubaiqing@vip.qq.com>
2022-01-11 10:31:31 +0800

    feat: 新增 1.11 日报

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -56,6 +56,11 @@ module.exports = {
     sidebar: [
       ["/", "1. 介绍"],
       {
+        title: "2022年01月",
+        collapsable: false,
+        children: readFileList("2022", "01")
+      },
+      {
         title: "2021年12月",
         collapsable: false,
         children: readFileList("2021", "12")
